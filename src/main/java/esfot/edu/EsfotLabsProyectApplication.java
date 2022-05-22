139eee90ae2f3848a514cc8d662fd01751725a64
Christian Palacios <cristhianpalacios3@gmail.com>
2022-05-22 17:18:25 -0500

    Initial commit

---
+++ src/main/java/esfot/edu/EsfotLabsProyectApplication.java
@@ -0,0 +1,11 @@
+package esfot.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsfotLabsProyectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EsfotLabsProyectApplication.class, args);
+	}
+}
---
+++ src/main/java/esfot/edu/controllers/controladorWeb.java
@@ -0,0 +1,14 @@
+package esfot.edu.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class controladorWeb {
+    /*
+    @GetMapping(value = "/index")
+    public String index() {
+        return "index";
+    }
+     */
+}
